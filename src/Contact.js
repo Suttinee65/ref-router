11a8df00a378b44ff091e5788fd5a0040a35fd56
Suttinee65 <suttinee13487@gmail.com>
2023-07-18 12:22:57 +0700

    react-create app

---
+++ src/Contact.js
@@ -0,0 +1,20 @@
+import { Layout } from "./App";
+
+function Contact() {
+    return (
+        <>
+        <Layout/>
+        <h3>Ubon Ratchathani University</h3>
+        <div>
+            85 Sathonlamark Road
+            <br />
+            Warin Chamrap District
+            <br />
+            Ubon Ratchathani
+            <br />
+            Thailand 34190
+        </div>
+        </>
+    );
+}
+export default Contact;
